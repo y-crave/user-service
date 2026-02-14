0d4f6419b37263822b2b8fa74ee2947b88adce07
pavel <pavel@>
2026-02-14 12:30:36 +0300

    add indexes and deleted deleted_at field

--- internal/migrations/000001_initial_migration.up.sql
+++ internal/migrations/000001_initial_migration.up.sql
@@ -5,7 +5,7 @@ CREATE TYPE target_name_enum AS ENUM ('for_like', 'for_one_day', 'for_all_time')
 
 CREATE TABLE targets (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name target_name_enum
+  name target_name_enum NOT_NULL
 );
 
 CREATE TYPE category_name_enum AS ENUM ('Interests', 'Worldview', 'Psychographer', 'Activity');
@@ -16,14 +16,13 @@ CREATE TABLE category (
     volume INTEGER,
     created_at timestamptz DEFAULT now(),
     updated_at timestamptz DEFAULT now(),
-    deleted_at timestamptz
 );
 
 CREATE TABLE users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  email VARCHAR(50),
+  email VARCHAR(255) UNIQUE,
   registration_state reg_state_enum,
-  name VARCHAR(20),
+  name VARCHAR(50),
   bio VARCHAR(500),
   birthdate timestamptz,
   sex sex_enum, 
@@ -32,20 +31,27 @@ CREATE TABLE users (
   target_id UUID,
   created_at timestamptz DEFAULT now(),
   updated_at timestamptz DEFAULT now(),
-  deleted_at timestamptz,
 
   CONSTRAINT fk_targets FOREIGN KEY (target_id) REFERENCES targets(id)
 );
 
 CREATE TABLE pictures (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  user_id UUID,
-  is_main BOOLEAN,
-  approved BOOLEAN,
-  path VARCHAR(225),
+  user_id UUID NOT NULL,
+  is_main BOOLEAN DEFAULT false,
+  approved BOOLEAN DEFAULT false,
+  path VARCHAR(255),
 
   CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_pictures_user_id ON pictures(user_id);
+
+-- У юзера может быть только одна главная фотка (is_main = true).
+-- Этот индекс не даст вставить вторую true, но разрешит много false.
+CREATE UNIQUE INDEX idx_pictures_one_main_per_user 
+ON pictures (user_id) 
+WHERE is_main IS TRUE;
 
 
 CREATE TABLE tags (
@@ -55,10 +61,12 @@ CREATE TABLE tags (
   category_id UUID, 
   created_at timestamptz DEFAULT now(),
   updated_at timestamptz DEFAULT now(),
-  deleted_at timestamptz,
 
   CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category(id)
 );
+
+CREATE INDEX idx_tags_category_id ON tags(category_id);
+CREATE INDEX idx_tags_name ON tags(name);
 
 CREATE TABLE user_tags (
     user_id UUID NOT NULL,
@@ -68,3 +76,4 @@ CREATE TABLE user_tags (
     CONSTRAINT fk_tag FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE
 );
 
+CREATE INDEX idx_user_tags_tag_id ON user_tags(tag_id);
