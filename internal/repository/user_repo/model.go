3c61c493534ad37c762aebc342531ccb812b273d
pavel <pavel@>
2026-02-14 12:16:33 +0300

    added gorm tags

--- internal/repository/user_repo/model.go
+++ internal/repository/user_repo/model.go
@@ -1,66 +1,65 @@
 package user_repo
 
 import (
-	"github.com/google/uuid"
 	"time"
-	"user-service/internal/domain"
+
+	"github.com/google/uuid"
 )
 
 type GormUser struct {
-	ID                uuid.UUID
-	Email             string
-	RegistrationState string `gorm:"type:reg_state_enum..." `
-	Name              string
-	BIO               string
-	Birthdate         time.Time
-	Sex               string
-	Location          string
-	Height            int
-	CreatedAt         time.Time
-	UpdatedAt         time.Time
-	DeletedAt         *time.Time
+	ID                uuid.UUID  `gorm:"type:uuid;primaryKey"`
+	RegistrationState string     `gorm:"type:reg_state_enum..." `
+	Name              string     `gorm:"type:varchar(100);not null"`
+	BIO               string     `gorm:"type:varchar(100);not null"`
+	Birthdate         time.Time  `gorm:"type:date;not null"`
+	Sex               string     `gorm:"type:sex;not null"`
+	Location          string     `gorm:"type:varchar(100);not null"`
+	Height            int        `gorm:"type:int"`
+	CreatedAt         time.Time  `gorm:"type:date;not null"`
+	UpdatedAt         time.Time  `gorm:"type:date;not null"`
+	DeletedAt         *time.Time // мне кажется это нам не надо
 }
 
 func (GormUser) TableName() string {
-	return "user"
+	return "users"
 }
 
 type GormPicture struct {
-	ID       uuid.UUID
-	UserID   uuid.UUID
-	IsMain   bool
-	Approved bool
-	Path     string
+	ID       uuid.UUID `gorm:"type:uuid;primaryKey"`
+	UserID   uuid.UUID `gorm:"type:uuid;primaryKey"`
+	IsMain   bool      `gorm:"not null"`
+	Approved bool      `gorm:"not null"`
+	Path     string    `gorm:"type:varchar(255);not null"`
 }
 
 func (GormPicture) TableName() string {
-	return "picture"
+	return "pictures"
 }
 
 type GormTarget struct {
-	ID   uuid.UUID
-	Name string
+	ID   uuid.UUID `gorm:"type:uuid;primaryKey"`
+	Name string    `gorm:"type:varchar(100);not null"`
 }
 
 func (GormTarget) TableName() string {
-	return "target"
+	return "targets"
 }
 
 type GormTag struct {
-	ID       uuid.UUID
-	Name     string
-	Volume   int
-	Category string
+	ID       uuid.UUID `gorm:"type:uuid;primaryKey"`
+	Name     string    `gorm:"type:varchar(100);not null"`
+	Volume   int       `gorm:"type:int;not null"`
+	Category string    `gorm:"type:varchar(100);not null"`
 }
 
 func (GormTag) TableName() string {
-	return "tag"
+	return "tags"
 }
 
 type GormCategory struct {
-	ID     uuid.UUID
-	Name   string
-	Volume int
+	ID     uuid.UUID `gorm:"type:uuid;primaryKey"`
+	Name   string    `gorm:"type:varchar(100);not null"`
+	Volume int       `gorm:"type:int;not null"`
 }
 
 func (GormCategory) TableName() string {
